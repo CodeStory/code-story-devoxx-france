90b9c66fb33a1cdfc34aeee5993a3909d9e0f584
David Gageot <david@gageot.net>
2012-04-19 14:47:03 +0200

    Template

--- src/main/java/CodeStoryResource.java
+++ src/main/java/CodeStoryResource.java
@@ -7,4 +7,18 @@ public class CodeStoryResource {
 	public File index() {
 		return new File("index.html");
 	}
+
+	@GET
+	@Path("mustache.js")
+	@Produces("application/javascript;charset=UTF-8")
+	public File mustache() {
+		return new File("mustache.js");
+	}
+
+	@GET
+	@Path("jquery.js")
+	@Produces("application/javascript;charset=UTF-8")
+	public File jquery() {
+		return new File("jquery.js");
+	}
 }
