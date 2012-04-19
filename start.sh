93e53414de1624cc5b6f18433fb06ae29e3c68c4
David Gageot <david@gageot.net>
2012-04-19 16:48:20 +0200

    Wait for the web server to start

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer
+mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer &
+
+wget --retry-connrefused --waitretry=5 "http://localhost:8080/"
