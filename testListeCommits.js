ea9c07eb80db3d7611958546a4d98c9f5460e702
David Gageot <david@gageot.net>
2012-04-19 13:58:36 +0200

    2 commits

--- testListeCommits.js
+++ testListeCommits.js
@@ -6,7 +6,8 @@ home = "http://localhost:" + process.env.PORT + "/";
 test("Checking Commits", function (done) {
         Browser.visit(home, function (e, browser) {
             expect(browser.text("title")).to.be("CodeStory - HomePage");
-            expect(browser.query("#commits")).to.be.ok();
+            expect(browser.query("#commits .commit:nth-child(1):contains('dgageot')")).to.be.ok();
+            expect(browser.query("#commits .commit:nth-child(2):contains('jlm')")).to.be.ok();
             done();
         });
     }
