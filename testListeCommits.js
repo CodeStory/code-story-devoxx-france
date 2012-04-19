b28d71b0d4be435a68fc7c2f3eb44337f9980826
David Gageot <david@gageot.net>
2012-04-19 15:02:22 +0200

    integrate gravatar url

--- testListeCommits.js
+++ testListeCommits.js
@@ -22,6 +22,9 @@ test("Checking Commits", function (done) {
             expect(browser.query("#commits .commit:nth-child(1) .date:contains('29/03/2012')")).to.be.ok();
             expect(browser.query("#commits .commit:nth-child(2) .date:contains('29/03/2012')")).to.be.ok();
 
+            expect(browser.query("#commits .commit:nth-child(1) img[src='https://secure.gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf']")).to.be.ok();
+            expect(browser.query("#commits .commit:nth-child(2) img[src='https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e']")).to.be.ok();
+
             done();
         });
     }
