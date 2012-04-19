6446b2281b54990973ec41fe5491e0feeeba5293
David Gageot <david@gageot.net>
2012-04-19 14:02:20 +0200

    I want the commit message in the page

--- testListeCommits.js
+++ testListeCommits.js
@@ -8,6 +8,8 @@ test("Checking Commits", function (done) {
             expect(browser.text("title")).to.be("CodeStory - HomePage");
             expect(browser.query("#commits .commit:nth-child(1):contains('dgageot')")).to.be.ok();
             expect(browser.query("#commits .commit:nth-child(2):contains('jlm')")).to.be.ok();
+            expect(browser.query("#commits .commit:nth-child(1) .message:contains('last commit')")).to.be.ok();
+            expect(browser.query("#commits .commit:nth-child(2) .message:contains('first commit')")).to.be.ok();
             done();
         });
     }
