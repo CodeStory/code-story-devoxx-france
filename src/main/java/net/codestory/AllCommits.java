15a0e7f3602d05853a0089fe2e6b7816fec55301
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-19 12:22:26 +0200

    Calling githubclient but not using it, wooooo

--- src/main/java/net/codestory/AllCommits.java
+++ src/main/java/net/codestory/AllCommits.java
@@ -1,5 +1,10 @@
 package net.codestory;
 
+import org.eclipse.egit.github.core.*;
+import org.eclipse.egit.github.core.client.*;
+import org.eclipse.egit.github.core.service.*;
+
+import java.io.*;
 import java.util.*;
 
 import static com.google.common.collect.Lists.*;
@@ -7,6 +12,16 @@ import static com.google.common.collect.Lists.*;
 public class AllCommits {
 
 	public List<Commit> list() {
+		GitHubClient githubClient = new GitHubClient("github", -1, "http");
+		RepositoryService repositoryService = new RepositoryService(githubClient);
+		CommitService commitService = new CommitService(githubClient);
+		Repository repository = null;
+		try {
+			repository = repositoryService.getRepository("dgageot", "CodeStory");
+			commitService.getCommits(repository);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return newArrayList(new Commit("dgageot", "first commit"), new Commit("jlmorlhon", "second commit"), new Commit("seblm", "third commit"));
 	}
 }
