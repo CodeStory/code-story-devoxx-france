a21ebff32fe8063ff6e0554655b10cfc3bccc653
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-19 12:02:30 +0200

    Using net.codestory package, pride to be there

---
+++ src/main/java/net/codestory/AllCommits.java
@@ -0,0 +1,12 @@
+package net.codestory;
+
+import java.util.*;
+
+import static com.google.common.collect.Lists.*;
+
+public class AllCommits {
+
+	public List<Commit> list() {
+		return newArrayList(new Commit("dgageot"), new Commit("jlmorlhon"), new Commit("seblm"));
+	}
+}
--- src/test/java/net/codestory/AllCommitsTest.java
+++ src/test/java/net/codestory/AllCommitsTest.java
@@ -1,4 +1,4 @@
-package net.gageot;
+package net.codestory;
 
 import org.junit.*;
 
