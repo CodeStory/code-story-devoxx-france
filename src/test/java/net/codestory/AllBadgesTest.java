95d0a86750018c00ae4c10da3651f8f303b3cad7
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-20 15:14:37 +0200

    Add a test for badges gravatar url

--- src/test/java/net/codestory/AllBadgesTest.java
+++ src/test/java/net/codestory/AllBadgesTest.java
@@ -15,5 +15,6 @@ public class AllBadgesTest {
 
 		assertThat(badges).onProperty("label").containsSequence("Top Committer", "Fatty Committer");
 		assertThat(badges).onProperty("image").containsSequence("/badges/topCommiter.png", "/badges/fatty.png");
+		assertThat(badges).onProperty("gravatarUrl").containsSequence("https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e", "https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e");
 	}
 }
