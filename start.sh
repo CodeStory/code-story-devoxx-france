1028d6d67415766050a712d3477fa99c6065e84c
David Gageot <david@gageot.net>
2012-04-20 10:51:42 +0200

    Fixing deploy scripts. again and again...

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 killall java
 
-mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer >/dev/null 2>&1 &
+nohup mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer < /dev/null > /dev/null 2>&1 &
 
 /usr/local/bin/wget -O - --inet4-only --retry-connrefused -t 10 --waitretry=5 "http://localhost:8080/" > /dev/null
+
+echo "Deploy successful"
+exit 0
