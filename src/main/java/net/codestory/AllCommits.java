96170eae6a59c92f8ed78ead8182f961f8430198
David Gageot <david@gageot.net>
2012-04-20 15:11:50 +0200

    Cache

--- src/main/java/net/codestory/AllCommits.java
+++ src/main/java/net/codestory/AllCommits.java
@@ -1,16 +1,45 @@
 package net.codestory;
 
 import com.google.common.base.*;
+import com.google.common.cache.*;
+import com.google.common.util.concurrent.*;
 import org.eclipse.egit.github.core.*;
 import org.eclipse.egit.github.core.client.*;
 import org.eclipse.egit.github.core.service.*;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class AllCommits {
 	private String user;
 	private String project;
+
+	LoadingCache<String, List<RepositoryCommit>> cache = CacheBuilder.newBuilder() //
+			.refreshAfterWrite(1, TimeUnit.MINUTES).build(new CacheLoader<String, List<RepositoryCommit>>() {
+				@Override
+				public List<RepositoryCommit> load(String key) throws Exception {
+					try {
+						System.out.println("GITHUB");
+						GitHubClient githubClient = new GitHubClient("github", -1, "http");
+						CommitService commits = new CommitService(githubClient);
+						RepositoryService repository = new RepositoryService(githubClient);
+						return commits.getCommits(repository.getRepository(user, project));
+					} catch (IOException e) {
+						throw Throwables.propagate(e);
+					}
+				}
+
+				@Override
+				public ListenableFuture<List<RepositoryCommit>> reload(final String key, List<RepositoryCommit> oldValue) throws Exception {
+					return ListenableFutureTask.create(new Callable<List<RepositoryCommit>>() {
+						@Override
+						public List<RepositoryCommit> call() throws Exception {
+							return load(key);
+						}
+					});
+				}
+			});
 
 	public AllCommits(String user, String project) {
 		this.user = user;
@@ -18,14 +47,6 @@ public class AllCommits {
 	}
 
 	public List<RepositoryCommit> list() {
-		GitHubClient githubClient = new GitHubClient("github", -1, "http");
-		CommitService commits = new CommitService(githubClient);
-		RepositoryService repository = new RepositoryService(githubClient);
-
-		try {
-			return commits.getCommits(repository.getRepository(user, project));
-		} catch (IOException e) {
-			throw Throwables.propagate(e);
-		}
+		return cache.getUnchecked("KEY");
 	}
 }
