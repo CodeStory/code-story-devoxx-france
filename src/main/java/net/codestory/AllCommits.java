ff25c423f470964a025efd468dc223b7dbe8fff7
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-20 14:34:56 +0200

    Remove unused class

--- src/main/java/net/codestory/AllCommits.java
+++ src/main/java/net/codestory/AllCommits.java
@@ -1,17 +1,31 @@
 package net.codestory;
 
+import com.google.common.base.*;
 import org.eclipse.egit.github.core.*;
+import org.eclipse.egit.github.core.client.*;
+import org.eclipse.egit.github.core.service.*;
 
+import java.io.*;
 import java.util.*;
 
 public class AllCommits {
-	private final AllGitHubCommits allGitHubCommits;
+	private String user;
+	private String project;
 
 	public AllCommits(String user, String project) {
-		allGitHubCommits = new AllGitHubCommits(user, project);
+		this.user = user;
+		this.project = project;
 	}
 
 	public List<RepositoryCommit> list() {
-		return allGitHubCommits.fetchCommitFromGitHub();
+		GitHubClient githubClient = new GitHubClient("github", -1, "http");
+		CommitService commits = new CommitService(githubClient);
+		RepositoryService repository = new RepositoryService(githubClient);
+
+		try {
+			return commits.getCommits(repository.getRepository(user, project));
+		} catch (IOException e) {
+			throw Throwables.propagate(e);
+		}
 	}
 }
