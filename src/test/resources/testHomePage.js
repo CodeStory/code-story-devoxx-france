599c7f94f38e8b65d08ba0fd57b6be3b13353737
David Gageot <david@gageot.net>
2012-04-21 19:44:21 +0200

    Simpler assertions

--- src/test/resources/testHomePage.js
+++ src/test/resources/testHomePage.js
@@ -12,13 +12,12 @@ test("Checking page title", function (done) {
 
 test("Checking commits", function (done) {
     Browser.visit(home, function (e, browser) {
-        expect(browser.query("#commits .commit:nth-child(1) .description:contains('message1')")).to.be.ok();
-        expect(browser.query("#commits .commit:nth-child(2) .description:contains('message2')")).to.be.ok();
-
+        expect(browser.query("#commits .commit:nth-child(1) :contains('message1')")).to.be.ok();
         expect(browser.query("#commits .commit:nth-child(1) img[src='url1']")).to.be.ok();
-        expect(browser.query("#commits .commit:nth-child(2) img[src='url2']")).to.be.ok();
-
         expect(browser.query("#commits .commit:nth-child(1) img[class='portrait SUCCESS']")).to.be.ok();
+
+        expect(browser.query("#commits .commit:nth-child(2) :contains('message2')")).to.be.ok();
+        expect(browser.query("#commits .commit:nth-child(2) img[src='url2']")).to.be.ok();
         expect(browser.query("#commits .commit:nth-child(2) img[class='portrait FAILURE']")).to.be.ok();
 
         done();
@@ -34,13 +33,13 @@ test("Checking project name", function (done) {
 
 test("Checking badges", function (done) {
     Browser.visit(home, function (e, browser) {
-        expect(browser.query("#badges .badge:nth-child(1) p:contains('Top Committer')")).to.be.ok();
+        expect(browser.query("#badges .badge:nth-child(1) :contains('Top Committer')")).to.be.ok();
         expect(browser.query("#badges .badge:nth-child(1) img[src='/badges/top.png']")).to.be.ok();
 
-        expect(browser.query("#badges .badge:nth-child(2) p:contains('Fatty Committer')")).to.be.ok();
+        expect(browser.query("#badges .badge:nth-child(2) :contains('Fatty Committer')")).to.be.ok();
         expect(browser.query("#badges .badge:nth-child(2) img[src='/badges/fatty.png']")).to.be.ok();
 
-        expect(browser.query("#badges .badge:nth-child(3) p:contains('Verbose Committer')")).to.be.ok();
+        expect(browser.query("#badges .badge:nth-child(3) :contains('Verbose Committer')")).to.be.ok();
         expect(browser.query("#badges .badge:nth-child(3) img[src='/badges/verbose.png']")).to.be.ok();
 
         done();
