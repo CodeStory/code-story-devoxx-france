379fcaef945dcd7f7cda7658560ed82ca71ceff4
David Gageot <david@gageot.net>
2012-04-23 09:26:20 +0200

    Extract constants

--- src/main/java/net/codestory/badges/AllBadges.java
+++ src/main/java/net/codestory/badges/AllBadges.java
@@ -1,7 +1,6 @@
 package net.codestory.badges;
 
 import com.google.inject.*;
-import groovy.lang.Binding;
 import groovy.lang.*;
 import net.codestory.github.*;
 import org.eclipse.egit.github.core.*;
@@ -9,11 +8,14 @@ import org.eclipse.egit.github.core.*;
 import java.util.*;
 
 import static com.google.common.base.Predicates.*;
-import static com.google.common.collect.ImmutableMap.*;
 import static java.util.Arrays.asList;
 import static net.gageot.listmaker.ListMaker.*;
 
 public class AllBadges {
+	private static final String TOP_COMMITTER = "(commits.groupBy { it.author?.login }.findAll { it.key != null }.max { it.value.size }?.value ?: [])[0]?.author";
+	private static final String FATTY_COMMITTER = "commits.findAll { it.stats != null }.max { it.stats.additions - it.stats.deletions }?.author ";
+	private static final String VERBOSE_COMMITTER = "commits.max { it.commit?.message ?: '' }?.author";
+
 	@Inject AllCommits allCommits;
 
 	public List<Badge> list() {
@@ -24,21 +26,22 @@ public class AllBadges {
 	}
 
 	User topCommitter() {
-		return (User) groovy("(commits.groupBy { it.author?.login }.findAll { it.key != null }.max { it.value.size }?.value ?: [])[0]?.author");
+		return (User) groovy(TOP_COMMITTER);
 	}
 
 	User fattyCommitter() {
-		return (User) groovy("commits.findAll { it.stats != null }.max { it.stats.additions - it.stats.deletions }?.author ");
+		return (User) groovy(FATTY_COMMITTER);
 	}
 
 	User verboseCommitter() {
-		return (User) groovy("commits.max { it.commit?.message ?: '' }?.author");
+		return (User) groovy(VERBOSE_COMMITTER);
 	}
 
 	Object groovy(String script) {
-		Binding bindings = new Binding(of("commits", allCommits.list()));
+		GroovyShell groovy = new GroovyShell();
+		groovy.setVariable("commits", allCommits.list());
 
-		return new GroovyShell(bindings).evaluate(script);
+		return groovy.evaluate(script);
 	}
 
 	static Badge badge(User user, String label, String image) {
