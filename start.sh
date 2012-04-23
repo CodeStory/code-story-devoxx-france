b5749c51a92c717cd3b756389124dff00983bf85
Romain Lespinasse <romain.lespinasse@gmail.com>
2012-04-23 20:17:33 +0200

    delete the annoying (... or not) nohup.out creation

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ killall java
 
 nohup mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer < /dev/null > /dev/null 2>&1 &
 
-/usr/local/bin/wget -O - --inet4-only --retry-connrefused -t 10 --waitretry=5 "http://localhost:8080/" > /dev/null
+/usr/local/bin/wget -O - --inet4-only --retry-connrefused -t 10 --waitretry=5 "http://localhost:8085/" > /dev/null
+
+rm nohup.out
 
 echo "Deploy successful"
 exit 0
