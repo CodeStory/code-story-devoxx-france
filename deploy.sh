9da0c7f39f27943173d4ecbb9c51b9bebd80ba91
David Gageot <david@gageot.net>
2012-04-23 21:43:47 +0200

    Simplify start script and remove deploy script

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -t david@jean-laurent.code-story.net "cd CodeStory; git fetch; git reset --hard origin/master; ./start.sh"
--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-killall java
-
-nohup mvn -q clean install -Dmaven.test.skip=true && java -classpath "target/lib/*:target/*" net.codestory.CodeStoryServer < /dev/null > /dev/null 2>&1 &
-
-/usr/local/bin/wget -O - --inet4-only --retry-connrefused -t 10 --waitretry=5 "http://localhost:8080/" > /dev/null
-
-echo "Deploy successful"
-exit 0
+mvn -q clean install -Dmaven.test.skip=true && java -cp "target/lib/*:target/*" net.codestory.CodeStoryServer
